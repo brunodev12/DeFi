dd3d3e25b6f1e41729638e459fd7dea4ddc0b734
brunodev12 <bruno.carthery12@gmail.com>
2023-01-12 12:00:44 -0300

    token creation

---
+++ contracts/JamToken.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract JamToken {
+
+    // Statements
+    string public name = "JAM Token";
+    string public symbol = "JAM";
+    uint256 public totalSupply = 1000000000000000000000000;
+    uint8 public decimals = 18;
+    
+    // Event for the transfer of tokens from a user
+    event Transfer (
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    // Event for the approval of an operator
+    event Approval (
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+    // Data structure
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
+
+    // Constructor
+    constructor(){
+        balanceOf[msg.sender] = totalSupply;
+    }
+}
