658af160c6d6a15a805135a92d031215e2c782f3
brunodev12 <bruno.carthery12@gmail.com>
2023-01-14 22:45:31 -0300

    staking function

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -24,4 +24,21 @@ contract TokenFarm {
         jamToken = _jamToken;
         owner = msg.sender;
     }
+
+    // Token staking
+    function stakeTokens(uint _amount) public {
+        // A quantity greater than 0 is required
+        require(_amount > 0, "The amount cannot be less than 0");
+        // Transfer Jam tokens to main smart contract
+        jamToken.transferFrom(msg.sender, address(this), _amount);
+        // Update staking balance
+        stakingBalance[msg.sender] += _amount;
+        // Save the user
+        if(!hasStaked[msg.sender]){
+            stakers.push(msg.sender);
+        }
+        // Update staking status
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+    }
 }
